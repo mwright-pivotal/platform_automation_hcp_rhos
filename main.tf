e7e51db53788b36e28a9d0d9ab2ad520a5bb52f5
Mike Wright <mike.wright@hashicorp.com>
2024-08-08 16:12:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
   decoded_yaml = yamldecode(file("${path.module}/apps/vm/basic_rhel9_vm.yaml"))
 }
 
-resource "kubernetes_manifest" "rhwl9_vm" {
+resource "kubernetes_manifest" "rhel9_vm" {
   for_each = { for k, v in local.decoded_yaml : k => v }
   manifest = each.value
 }
