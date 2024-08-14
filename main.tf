26de7877f160fb23a2b5f46a8b9f861300f29342
Mike Wright <mike.wright@hashicorp.com>
2024-08-14 08:03:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "kubernetes_manifest" "rook-common" {
 resource "kubernetes_manifest" "rook-operator-openshift" {
   for_each = { for k, v in provider::kubernetes::manifest_decode_multi(file("${path.module}/infra/rook/operator-openshift.yaml")) : k => v }
   manifest = each.value
+  field_manager {
+    # set the name of the field manager
+    name = "myteam"
+
+    # force field manager conflicts to be overridden
+    force_conflicts = true
+  }
 }
 
 resource "kubernetes_manifest" "rook-cluster" {
