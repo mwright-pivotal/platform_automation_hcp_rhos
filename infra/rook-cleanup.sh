8124f956f528eaff44197a32b283e14d3babd5f4
Mike Wright <mike.wright@hashicorp.com>
2024-08-14 11:18:26 -0400

    Update rook-cleanup.sh to reset disks

--- infra/rook-cleanup.sh
+++ infra/rook-cleanup.sh
@@ -11,3 +11,8 @@ kubectl delete -f rook/operator-openshift.yaml
 ssh $HOST3 "sudo rm -r -f  /var/lib/rook/*"
 ssh $HOST2 "sudo rm -r -f  /var/lib/rook/*"
 ssh $HOST1 "sudo rm -r -f  /var/lib/rook/*"
+
+#danger!  make sure you wipe the correct disks
+ssh $HOST3 "sudo wipefs -a /dev/sda"
+ssh $HOST2 "sudo wipefs -a /dev/sdb"
+ssh $HOST1 "sudo wipefs -a /dev/sdb"
