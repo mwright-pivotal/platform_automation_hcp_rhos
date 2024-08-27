3cfbccde3b19071a5b35e18a0c6e55eeed052807
mwright-pivotal <michael-mw.wright@broadcom.com>
2024-08-27 21:51:55 +0000

    remove yaml parsing

--- main.tf
+++ main.tf
@@ -27,50 +27,6 @@ module "rook" {
    source = "./infra/rook"
 }
 
-resource "kubernetes_manifest" "rook-common" {
-  for_each = { for k, v in provider::kubernetes::manifest_decode_multi(file("${path.module}/infra/rook/common.yaml")) : k => v }
-  manifest = each.value
-}
-
-resource "kubernetes_manifest" "rook-operator-openshift" {
-  for_each = { for k, v in provider::kubernetes::manifest_decode_multi(file("${path.module}/infra/rook/operator-openshift.yaml")) : k => v }
-  manifest = each.value
-  field_manager {
-    # set the name of the field manager
-    name = "myteam"
-
-    # force field manager conflicts to be overridden
-    force_conflicts = true
-  }
-}
-
-resource "kubernetes_manifest" "rook-cluster" {
-  manifest = provider::kubernetes::manifest_decode(file("${path.module}/infra/rook/cluster.yaml"))
-  field_manager {
-    # set the name of the field manager
-    name = "myteam"
-
-    # force field manager conflicts to be overridden
-    force_conflicts = true
-  }
-}
-
-resource "kubernetes_manifest" "rook-filesystem" {
-  for_each = { for k, v in provider::kubernetes::manifest_decode_multi(file("${path.module}/infra/rook/filesystem.yaml")) : k => v }
-  manifest = each.value
-  field_manager {
-    # set the name of the field manager
-    name = "myteam"
-
-    # force field manager conflicts to be overridden
-    force_conflicts = true
-  }
-}
-
-resource "kubernetes_manifest" "rook-toolbox" {
-  manifest = provider::kubernetes::manifest_decode(file("${path.module}/infra/rook/toolbox.yaml"))
-}
-
 resource "kubernetes_manifest" "vault" {
   for_each = { for k, v in provider::kubernetes::manifest_decode_multi(file("${path.module}/infra/vault/install.yaml")) : k => v }
   manifest = each.value
