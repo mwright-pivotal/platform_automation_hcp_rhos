83ff1195fba1e31ce79ad2c3f0fadb936ddcf9a5
mwright-pivotal <michael-mw.wright@broadcom.com>
2024-09-10 16:21:39 +0000

    node feature instance

--- infra/nvidia/nfd-module/nfd-operatorgroup.tf
+++ infra/nvidia/nfd-module/nfd-operatorgroup.tf
@@ -13,7 +13,7 @@ resource "kubernetes_manifest" "namespace_openshift_nfd" {
 }
 
 resource "kubernetes_manifest" "operatorgroup_openshift_nfd_openshift_nfd" {
-  depends_on = ["kubernetes_manifest.namespace_openshift_nfd"]
+  depends_on = [kubernetes_manifest.namespace_openshift_nfd]
   manifest = {
     "apiVersion" = "operators.coreos.com/v1"
     "kind" = "OperatorGroup"
@@ -31,7 +31,7 @@ resource "kubernetes_manifest" "operatorgroup_openshift_nfd_openshift_nfd" {
 }
 
 resource "kubernetes_manifest" "subscription_openshift_nfd_nfd" {
-  depends_on = ["kubernetes_manifest.namespace_openshift_nfd"]
+  depends_on = [kubernetes_manifest.namespace_openshift_nfd]
   manifest = {
     "apiVersion" = "operators.coreos.com/v1alpha1"
     "kind" = "Subscription"
@@ -45,6 +45,101 @@ resource "kubernetes_manifest" "subscription_openshift_nfd_nfd" {
       "name" = "nfd"
       "source" = "redhat-operators"
       "sourceNamespace" = "openshift-marketplace"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "nodefeaturediscovery_openshift_nfd_nfd_instance" {
+  depends_on = [kubernetes_manifest.operatorgroup_openshift_nfd_openshift_nfd]
+  manifest = {
+    "apiVersion" = "nfd.openshift.io/v1"
+    "kind" = "NodeFeatureDiscovery"
+    "metadata" = {
+      "name" = "nfd-instance"
+      "namespace" = "openshift-nfd"
+    }
+    "spec" = {
+      "customConfig" = {
+        "configData" = <<-EOT
+        - name: "more.kernel.features"
+          matchOn:
+          - loadedKMod: ["example_kmod3"]
+        
+        EOT
+      }
+      "instance" = ""
+      "operand" = {
+        "image" = "registry.redhat.io/openshift4/ose-node-feature-discovery-rhel9:v4.16.0"
+        "imagePullPolicy" = "Always"
+      }
+      "topologyupdater" = false
+      "workerConfig" = {
+        "configData" = <<-EOT
+        core:
+        #  labelWhiteList:
+        #  noPublish: false
+          sleepInterval: 60s
+        #  sources: [all]
+        #  klog:
+        #    addDirHeader: false
+        #    alsologtostderr: false
+        #    logBacktraceAt:
+        #    logtostderr: true
+        #    skipHeaders: false
+        #    stderrthreshold: 2
+        #    v: 0
+        #    vmodule:
+        ##   NOTE: the following options are not dynamically run-time configurable
+        ##         and require a nfd-worker restart to take effect after being changed
+        #    logDir:
+        #    logFile:
+        #    logFileMaxSize: 1800
+        #    skipLogHeaders: false
+        sources:
+          cpu:
+            cpuid:
+        #     NOTE: whitelist has priority over blacklist
+              attributeBlacklist:
+                - "BMI1"
+                - "BMI2"
+                - "CLMUL"
+                - "CMOV"
+                - "CX16"
+                - "ERMS"
+                - "F16C"
+                - "HTT"
+                - "LZCNT"
+                - "MMX"
+                - "MMXEXT"
+                - "NX"
+                - "POPCNT"
+                - "RDRAND"
+                - "RDSEED"
+                - "RDTSCP"
+                - "SGX"
+                - "SSE"
+                - "SSE2"
+                - "SSE3"
+                - "SSE4.1"
+                - "SSE4.2"
+                - "SSSE3"
+              attributeWhitelist:
+          kernel:
+            kconfigFile: "/path/to/kconfig"
+            configOpts:
+              - "NO_HZ"
+              - "X86"
+              - "DMI"
+          pci:
+            deviceClassWhitelist:
+              - "0200"
+              - "03"
+              - "12"
+            deviceLabelFields:
+              - "class"
+        
+        EOT
+      }
     }
   }
 }
